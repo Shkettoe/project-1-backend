a8a07f8d1e5a8477c6bc260bcdbdf442fbbbab2a
Shkettoe <103437209+Shkettoe@users.noreply.github.com>
2022-05-14 15:22:07 +0200

    enabled cors for frontend

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import * as cookieparser from 'cookie-parser'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieparser())
+  app.enableCors({origin: 'http://localhost:3000'})
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }))
